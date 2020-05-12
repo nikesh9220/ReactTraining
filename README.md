40a563b27bf9d9c9e712e983f2555a8a7573d3d2
Nikesh Pandya <Nikesh.Pandya@civica2.com>
2020-05-12 12:18:28 +0530

    React Training

---
+++ README.md
@@ -0,0 +1,64 @@
+This Repository contain solution of below problem statements.
+Date 11 May 2020
+<ol>
+    <li>Working With List and Conditions</li>
+    <li>Rendering Content conditionally</li>
+    <li>List & States</li>
+    <li>List & Keys</li>
+</ol>
+
+Date 10 May 2020
+<ol>
+    <li>Stateless and Statefull Component</li>
+    <li>Two Way binding</li>
+    <li>Styling the component</li>
+</ol>
+
+Date 09 May 2020
+<ol>
+    <li>Working with props</li>
+    <li>States</li>
+    <li>Event Handling</li>
+    <li>manipulating State</li>
+</ol>
+
+Date 08 May 2020
+<ol>
+    <li>Base Features and Syntax</li>
+    <li>Folder Structure</li>
+    <li>Introduction to JSX</li>
+    <li>Generate Static Content</li>
+    <li>Generate Dynamic Content Content</li>
+</ol>
+
+Date 06 May 2020
+<ol>
+    <li>Create React App using command</li>
+    <li>Basic of React</li>
+    <li>Arrow Function (ES6)</li>
+</ol>
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br>
+You will also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.<br>
+See the section about [running tests](#running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br>
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br>
+Your app is ready to be deployed!
+
---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { id:'N1', name: 'Max', age: 28 },
+      { id:'N2', name: 'Manu', age: 29 },
+      { id:'N3', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  }
+
+  // switchNameHandler = (newName) => {
+  //   // console.log('Was clicked!');
+  //   // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+  //   this.setState( {
+  //     persons: [
+  //       { name: newName, age: 28 },
+  //       { name: 'Manu', age: 29 },
+  //       { name: 'Stephanie', age: 27 }
+  //     ],
+  //     showName : false
+  //   } )
+  // }
+
+  nameChangedHandler = (event,personIndex) => {
+    const _personIndex = this.state.persons.findIndex(p =>{
+        return p.id === personIndex
+    });
+    debugger;
+    const _person = {
+      ...this.state.persons[_personIndex] //Copy object using spread operator
+    }
+    _person.name = event.target.value;
+    
+    const _persons = [...this.state.persons];
+    _persons[_personIndex] = _person;
+
+    this.setState( {
+      persons:_persons
+    } )
+  }
+  toggleNameHandler =() =>{
+    const showNameState = this.state.showName;
+    this.setState({
+      showName : ! showNameState
+    })
+  }
+  
+deleteNameHandler = (personIndex) =>{
+  debugger;
+  //const _persons=this.state.persons; // don not use like this it is called mutable state bcoz we are refrencing to thT ARRAY
+  //unmutable state changing
+  //We can also use const _persons=this.state.persons.slice() it will copy array
+  const _persons = [...this.state.persons] //Unmutable state copy array by ES6 Spread operator.
+  _persons.splice(personIndex,1)
+  this.setState({
+    persons:_persons
+  })
+}
+  render () {
+    let persons = null;
+    if(this.state.showName){
+        persons= 
+        <div>
+          {
+          this.state.persons.map((person,index) =>{
+              return <Person click={() =>this.deleteNameHandler(index)} Key={person.id} name={person.name} age={person.age} 
+              changed={(event) => this.nameChangedHandler(event,person.id)}/>
+          })
+          }
+        
+        </div> 
+        
+    }
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button 
+          style={style}
+          onClick={this.toggleNameHandler}>Toggle Name</button>
+        
+      {persons}
+        
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
+}
+
+export default App;
